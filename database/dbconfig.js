bed3d8cc64cdf3089dab4a652fddd83834a91ae4
Jason Long <jlong5795@gmail.com>
2021-08-03 14:15:27 -0500

    trying

--- database/dbconfig.js
+++ database/dbconfig.js
@@ -3,6 +3,6 @@ const knex = require('knex');
 const knexfile = require('../knexfile');
 
 const env = process.env.NODE_ENV || 'development';
-const configOptions = knexfile['production'];
+const configOptions = knexfile[env];
 
 module.exports = knex(configOptions);
