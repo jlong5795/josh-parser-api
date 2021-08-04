9fe692a79d50fd97cd6783cd55ad9daf954c5623
Jason Long <jlong5795@gmail.com>
2021-08-04 10:30:48 -0500

    cors

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const router = require('./router');
 
 const server = express();
 
+let whitelist = ['https://optimistic-torvalds-d28ab6.netlify.app/', 'https://netlify.app/']
+let corsOptions = {
+    origin: whitelist
+}
+
 // middleware
 server.use(express.json());
-server.use(cors());
+server.use(cors(corsOptions));
 
 // routes
 server.use('/data', router);
